789e17fcf7726c360b6db6bb414aab923906f389
Sangeetha_Natarajan <sarathisangee@gmail.com>
2021-08-09 13:12:33 +0530

    test commit for pull class is updated

---
+++ src/test/java/com/report/demo/TestCommitForPull.java
@@ -0,0 +1,13 @@
+package com.report.demo;
+
+import org.testng.annotations.Test;
+
+public class TestCommitForPull {
+	
+	@Test
+	public void test_method()
+	{
+		System.out.println("test method");
+	}
+
+}
